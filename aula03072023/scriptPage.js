cce13f63d5ce4ff0899fb6ef2c4325ef342c81fd
DougBraz <intertrade.douglas@gmail.com>
2023-07-03 15:44:41 -0300

    atividade230623.html

---
+++ aula03072023/scriptPage.js
@@ -0,0 +1,1 @@
+alert ("execultado JS no navegador")
---
+++ aula03072023/scriptnoPage.js
@@ -0,0 +1,13 @@
+let nome
+nome = "Douglas Roberto"
+console.log (nome)
+let idade = 31
+console.log (idade)
+let altura = 1.63
+console.log (altura)
+let CPF = "406789321-00"
+console.log (CPF)
+let ecasado = false //false e true
+console.log (ecasado)
+nome = "Daniel Cardoso"
+console.log (nome)
